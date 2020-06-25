0a6da1dffe2e6fd4da52d88e85794b115abec73f
Adachi Sakura <805983409@qq.com>
2020-06-25 14:33:42 +0800

    refactor(json object): remove redundant dependency

--- GeneticSerializableObjects.cpp
+++ GeneticSerializableObjects.cpp
@@ -2,7 +2,6 @@
 // Created by Administrator on 2020/5/11.
 //
 #include "Genetic.h"
-#include "thirdparty/jsonxx/jsonxx.h"
 using namespace jsonxx;
 
 void AlgorithmParameters::unserialize(const std::string & json)
